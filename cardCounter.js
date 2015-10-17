edf6fd90e8a8be4967a126b8d36df3ba8e2e94df
r36huang <r36huang@uwaterloo.ca>
2015-10-17 18:23:04 +0800

    changed trigger msg and enable alerts

--- cardCounter.js
+++ cardCounter.js
@@ -1,5 +1,5 @@
 // ==UserScript==
-// @name         TichuIQ Card Counter
+// @name         TichuIQ Card Counter with Alert
 // @namespace    http://your.homepage/
 // @version      0.1
 // @description  enter something useful
@@ -15,6 +15,8 @@
 // To make this automatically load, install this script into Tampermonkey for Google Chrome
 // When you type ":]" in the chat, the script will start telling you what cards have not yet been played, and when you type ":[" it will tell you the unplayed cards sorted by suit.
 // To stop it from telling you the unplayed cards, just type anything else in chat.
+// When you type "[:" in the chat, the script will print out what cards have not yet been played in the alert box, and when you type "]:" it will send an alert with the unplayed cards sorted by suit.
+// This will trigger a flag that you have already been alerted. Type " " in the chat to clear the flag.
 var cards = {}; 
 var lp = []; 
 var allCards = ["00", "01", "02a", "02b", "02c", "02d", "03a", "03b", "03c", "03d", "04a", "04b", "04c", "04d", "05a", "05b", "05c", "05d", "06a", "06b", "06c", "06d", "07a", "07b", "07c", "07d", "08a", "08b", "08c", "08d", "09a", "09b", "09c", "09d", "10a", "10b", "10c", "10d", "11a", "11b", "11c", "11d", "12a", "12b", "12c", "12d", "13a", "13b", "13c", "13d", "14a", "14b", "14c", "14d", "15", "17"]
@@ -28,6 +30,7 @@ function resetCardsBySuit(cardsBySuit) {
 	allCardsBySuit.forEach(function(c) {cardsBySuit[c] = false;});
 }
 resetCardsBySuit(cardsBySuit);
+var alerted = false;
 setInterval(function(){
 	$.get('http://tichuiq.com/public_html/get_game_data.php?start=0&firstcall=0').then(function(r){
 			var r = JSON.parse(r); 
@@ -38,8 +41,11 @@ setInterval(function(){
 			var np = Object.keys(cards).filter(function(c){return cards[c];}); 
 			(lp.length < np.length) && console.log(np);
 			lp = np; 
-			if(r.messages[0] && r.messages[0].m == "oh dear god") {getCards(0);};
-			if(r.messages[0] && r.messages[0].m == "oh deer god") {getCardsBySuit(0);};
+			if(r.messages[0] && r.messages[0].m == ":]") {getCards(0);};
+			if(r.messages[0] && r.messages[0].m == ":[") {getCardsBySuit(0);};
+			if(r.messages[0] && r.messages[0].m == "[:" && !alerted) {alert(Object.keys(cardsBySuit).filter(function(c){return !cards[c];})); alerted = true;};
+			if(r.messages[0] && r.messages[0].m == "]:" && !alerted) {alert(Object.keys(cardsBySuit).filter(function(c){return !cardsBySuit[c];})); alerted = true;};
+			if(r.messages[0] && r.messages[0].m == " ") {alerted = false;};
 		})
 }, 500)
 function getCards(i) {
