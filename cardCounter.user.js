6d797a1b093afb5d327f814c91fdbe83ddc1a826
Pyrotekniq <Pyrotekniq@users.noreply.github.com>
2015-10-30 07:33:28 -0400

    Update cardCounter.user.js

--- cardCounter.user.js
+++ cardCounter.user.js
@@ -84,17 +84,7 @@ function createWindow(){
 	t1.appendChild(t2);
 	cardWindow.document.body.appendChild(t1);
 	//;document.getElementsByTagName("BODY")[0]
-	return cardWindow;
 	
-}
-
-function updateWindow(cardWindow, cards){
-
-	var cardDocument = cardWindow.document;
-	var bodyElement;
-	var bodyText;
-	var br = cardDocument.createElement("BR");
-
 	for(i = 0; i<56; i++){
 		
 		bodyElement = cardDocument.createElement("P");
@@ -102,6 +92,21 @@ function updateWindow(cardWindow, cards){
 		bodyElement.appendChild(bodyText);
 		cardDocument.body.appendChild(bodyElement);
 		cardDocument.body.appendChild(br);
+		
+	}
+	
+	
+	return cardWindow;
+	
+}
+
+function updateWindow(cardWindow, cards){
+
+	var cardDocument = cardWindow.document;
+
+	for(i = 0; i<56; i++){
+		
+		cardDocument.body.getElementsByTagName("P")[i].innerHTML = cards[i];
 		
 	}
 
